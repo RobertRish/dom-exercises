bc6314e946e71a00dbdb1b6372c04b178554fd03
RobertRish <robert.rish1225@gmail.com>
2022-10-20 18:15:50 -0400

    complete step p 1

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// Step 1 a <p> with red text that says “Hey I’m red!”
+const pRed = document.createElement('p');
+pRed.textContent = "Hey I'm red!";
+pRed.style.cssText = "color: red;";
+container.appendChild(pRed);
